4d83f83a3195aa92a66701ba0a7ced34cf6c6ee8
yeniferwz <yeniferwuzhang@example.com>
2022-12-05 12:43:07 +0100

    user controller

--- web/trivial5/database/migrations/2014_10_12_000000_create_users_table.php
+++ web/trivial5/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->bigInteger('tota_score')->default(0);
+            $table->bigInteger('total_score')->default(0);
             $table->timestamps();
         });
     }
