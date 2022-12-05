36d7b1ab9a86f43ba4821d3dad212ec94da5a3a1
Iker Bravo <a20ikebraper@inspedralbes.cat>
2022-12-05 13:10:42 +0100

    Ranking funcional

--- web/trivial5/routes/web.php
+++ web/trivial5/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\PlayedgameController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
@@ -33,6 +34,8 @@ Route::get('/',function(){
 });
 
 Route::get('demo/{id}', [GameController::class, 'index_jugar'] );
+
+Route::get('ranking', [UserController::class, 'index'] );
 
 Route::post('savegame', [GameController::class, 'store']);
 Route::post('saveresult', [PlayedgameController::class, 'store']);
