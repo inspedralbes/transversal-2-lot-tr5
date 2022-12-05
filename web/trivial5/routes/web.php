1525754686ec37572578362d06bf0c7599d1e63e
yeniferwz <yeniferwuzhang@example.com>
2022-12-05 12:36:58 +0100

    afegir puntuacion total para cada user

--- web/trivial5/routes/web.php
+++ web/trivial5/routes/web.php
@@ -18,14 +18,18 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+Route::get('/',function(){
+    return view('welcome');
 });
 
 Route::get('demo/{id}', [GameController::class, 'index_jugar'] );
