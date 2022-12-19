85448f38c2f3028e3b9b6f49746be14c0d3e001e
yeniferwz <yeniferwuzhang@example.com>
2022-12-19 10:30:54 +0100

    css

--- web/js/utils.js
+++ web/js/utils.js
@@ -1168,20 +1168,21 @@ Vue.component('game' , {
                         <b-button @click="decreasePage" class="button__play"><span>Back</span></b-button>
                         <b-button @click="startGame" class="button__play"><span>Start</span></b-button>
                     </div>
-                    
-                    <div class="game__div">
-                        <div v-if="showQuestions" v-for="(question, index) in this.questions" class="game__body">
-                            <question v-show="actualQuestion == index" :infoQuestion="question" @userAnswer="addUserAnswer">
-                            <br><br>
-                            <div v-for="(answer, index) in userAnswers" class="game__cardFooter">
-                                <div v-if="isLogged" v-bind:class="{ game__answerCorrect:  comprobarRespuestaCorrecta(index), game__answerIncorrect: comprobarRespuestaIncorrecta(index)}">{{index+1}}</div>
+                    <div class="game__outerDiv">
+                        <div class="game__div">
+                            <div v-if="showQuestions" v-for="(question, index) in this.questions" class="game__body">
+                                <question v-show="actualQuestion == index" :infoQuestion="question" @userAnswer="addUserAnswer">
+                                <br><br>
+                                <div v-for="(answer, index) in userAnswers" class="game__cardFooter">
+                                    <div v-if="isLogged" v-bind:class="{ game__answerCorrect:  comprobarRespuestaCorrecta(index), game__answerIncorrect: comprobarRespuestaIncorrecta(index)}">{{index+1}}</div>
+                                </div>
+                                <br><br>
+                                <h3>Timer: {{timer}}</h3>
+                                </question>
                             </div>
-                            <br><br>
-                            <h3>Timer: {{timer}}</h3>
-                            </question>
                         </div>
                     </div>
-                    <div v-if="showQuestions">
+                    <div v-if="showQuestions"></div>
                         
                     <div v-if="showResults">
                         <results :results=userAnswers :timerRestante=timer :daily=daily :difficulty=selectedDifficulty @saveData="updateScore" @playagain="playagain"></results>
